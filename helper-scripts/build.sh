a93ee1fe2fda8e40f8a7fce846e8d86dcbbdaad3
Bradley Schofield <bradley@appwrite.io>
2022-09-26 14:59:25 +0100

    Update build.sh

--- helper-scripts/build.sh
+++ helper-scripts/build.sh
@@ -5,12 +5,12 @@
 COMPARCH=$(uname -m)
 
 # Clean up a couple conversion errors of uname to rust toolchain
-if [ $COMPARCH="arm64" ];
+if [ "$COMPARCH" = "arm64" ]
 then
     COMPARCH=aarch64
 fi
 
-if [ $COMPARCH = "armv7l" ]
+if [ "$COMPARCH" = "armv7l" ]
 then
     echo "Building PHP-Scrypt for armv7"
     cargo build --release --target=armv7-unknown-linux-gnueabihf
